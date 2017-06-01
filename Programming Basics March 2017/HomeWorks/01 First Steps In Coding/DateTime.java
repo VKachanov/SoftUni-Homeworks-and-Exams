e127fb43565df23e8bd41822b2a20acd6b9772e1
VKachanov <v_kachanov@abv.bg>
2017-06-01 17:31:48 +0300

    Added files to homeworks

---
+++ Programming Basics March 2017/HomeWorks/01 First Steps In Coding/DateTime.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class DateTime {
+    public static void main (String [] args) {
+        Date d = new Date ();
+        System.out.println(d);
+    }
+}
---
+++ Programming Basics March 2017/HomeWorks/01 First Steps In Coding/SQRT.java
@@ -0,0 +1,7 @@
+
+public class SQRT {
+    public static void main (String[] args){
+        System.out.println(Math.sqrt(12345));
+    }
+
+}
